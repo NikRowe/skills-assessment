abd83a0fc5cfe42c28c22c6d096291b97b0e080f
Nik Rowe <NikRowe7@Gmail.com>
2020-10-15 03:54:06 -0700

    Contacts Data and CSS Grid Fine Tunes

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const CONTACTS = [
+    {
+        id: 0,
+        name: "Christian",
+        status: "🟢",
+        email: "christian@yahoo.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 1,
+        name: "Rich",
+        status: "🟢",
+        email: "rich@tripod.com",
+        phone: "323-555-1234",
+        address: "1600 Pennsylvania Av NW, Washington, DC 20500"
+    },
+    {
+        id: 2,
+        name: "Scott",
+        status: "🟢",
+        email: "scott@mailinator.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 3,
+        name: "Danny",
+        status: "🟢",
+        email: "danny@hotmail.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 4,
+        name: "Taka",
+        status: "🔴 ",
+        email: "taka@myspace.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 5,
+        name: "Tim",
+        status: '🟡',
+        email: "tim@netscape.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 6,
+        name: "Patrick",
+        status: "🟢",
+        email: "patrick@live.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    },
+    {
+        id: 7,
+        name: "Jacques",
+        status: "🟡",
+        email: "jacques@aol.com",
+        phone: "323-555-1234",
+        address: "244 W. Main, Spokane, WA 99201"
+    }
+];
